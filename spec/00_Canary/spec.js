0d5ff526bfaa056cbd8a8a664ec65c2ab88c1b0d
Danya D. Smith <danyadsmith@gmail.com>
2020-04-29 00:21:07 -0400

    Chore: Validate Test Suite with Canary Test
    This commit adds a canary test to validate that the test suite is
    running and returns expected results.

---
+++ spec/00_Canary/spec.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('[CANARY TEST]', function () {
+  describe('Mocha/Chai Test Environment', function () {
+    it('runs', function () {
+      expect(false).to.equal(false);
+    });
+  });
+});
